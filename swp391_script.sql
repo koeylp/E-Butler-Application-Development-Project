798bd5dcad88aa283d4edc45f43f6a445bc0a242
TrongToann <nguyentrongtoan9a1nbk@gmail.com>
2022-11-03 21:00:36 +0700

    update sql

--- swp391_script.sql
+++ swp391_script.sql
@@ -1962,4 +1962,5 @@ SELECT * FROM tblUser
 SELECT * FROM tblDelivery
 SELECT * FROM tblOrder
 SELECT * FROM tblOrder_Product_Detail
-
+SELECT DISTINCT Ord.order_ID, Ord.order_Date, Ord.customer_ID, Ord.status, Ord.total ,  PD.provider_ID FROM ( tblOrder Ord JOIN tblOrder_Product_Detail OrdP ON Ord.order_ID = OrdP.order_ID ) JOIN tblProductDetail PD ON PD.id = OrdP.product_detail_ID WHERE PD.provider_ID = ?
+SELECT De.order_id, De.address,De.username_Shipper ,Ord.order_Date,Ord.customer_ID, Cus.name,Ord.total, De.status FROM (tblDelivery De JOIN tblOrder Ord ON De.order_id = Ord.order_ID) JOIN tblCustomer Cus ON Cus.username = Ord.customer_ID  
